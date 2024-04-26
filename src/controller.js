82d068f3552006ad3644b3d42a8a13ba71ac2f6b
darien rosero <darien.rosero2003@gmail.com>
2024-04-25 23:35:02 -0500

    {caracteristica}: configuracion de multer

---
+++ src/controller.js
@@ -0,0 +1,3 @@
+export const uprole = async (req, res) => {
+  res.send('imagen subida')
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,45 @@
 import express from 'express'
+import multer from 'multer'
+import { uprole } from './controller.js'
+import { PORT } from './config.js'
 
 const app = express()
 
-app.listen(4000, () => console.log('server runnig on http://localhost:4000'))
+// funcion pra almacenar archivos
+const storage = multer.diskStorage({
+
+  destination: function (req, file, cb) {
+    cb(null, './profiles')
+  },
+  // agregar fecha a los archivos subidos
+  filename: function (req, file, cb) {
+    const date = Date.now()
+    cb(null, `${date}-${file.originalname}`)
+  }
+
+})
+// filtra archivoa
+const fileFilter = (req, file, cb) => {
+  const permitidos = ['image/jpeg', 'image/png']
+  if (permitidos.includes(file.mimetype)) {
+    cb(null, true)
+  } else {
+    cb(new Error('solo se permiten imagenes'), false)
+  }
+}
+
+const filerole = multer({ storage, fileFilter })
+
+// url para subir imagen
+app.post('/uprole', filerole.single('profile'), uprole)
+
+// captura el erro de multer
+app.use((err, req, res, next) => {
+  if (err.message) {
+    return res.status(400).json({ message: err.message })
+  } else {
+    return res.status(500).json({ message: 'huvo un error interno' })
+  }
+})
+
+app.listen(PORT, () => console.log(`server runnig on http://localhost: ${4000}`))
